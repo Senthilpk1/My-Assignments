9a0eabefecd5a0706957fed9d81096ff2d91e776
Senthilpk1 <senthilpk1@gmail.com>
2024-07-23 17:45:40 +0530

    Week1day3

---
+++ EvenNumbers.java
@@ -0,0 +1,17 @@
+package Week1day3;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i =21; i <=30; i++) {
+			if (i%2==0){
+				System.out.println(i);	
+			}
+		}
+				
+	}
+
+	}
+
+
